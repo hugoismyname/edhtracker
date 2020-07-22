8cb044c3701968187e592e61e8b5435f86828f2f
Hugo Caruso <hugoismyname@yahoo.com>
2020-07-22 13:21:03 -0400

    "moved some of users forms and models"

--- edhtracker/users/admin.py
+++ edhtracker/users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
+from users.models import Profile
 from edhtracker.users.forms import (
     UserChangeForm,
     UserCreationForm,
@@ -9,6 +10,7 @@ from edhtracker.users.forms import (
 
 User = get_user_model()
 
+admin.site.register(Profile)
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
