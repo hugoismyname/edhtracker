8f848c62c1f9dd92a075359b98e72c2c2c19fcdb
Hugo Caruso <hugoismyname@yahoo.com>
2020-09-10 16:47:24 -0400

    heroku mailgun

--- config/settings/production.py
+++ config/settings/production.py
@@ -75,6 +75,7 @@ EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 ANYMAIL = {
     "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
     "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.mailgun.net/v3"),
+    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_SENDER_DOMAIN"),
 }
 
 
