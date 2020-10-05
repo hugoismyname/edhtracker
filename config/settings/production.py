abae62bcc7c0d2b84c6f6791b095e1a275925d22
Hugo Caruso <hugoismyname@yahoo.com>
2020-10-05 15:08:44 -0400

    added aws env variables

--- config/settings/production.py
+++ config/settings/production.py
@@ -31,6 +31,9 @@ CACHES = {
 
 # AWS
 # ------------------------------------------------------------------------------
+AWS_ACCESS_KEY_ID = env.("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env.("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env.("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_FILE_OVERWRITE = None
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
