362063b27acdaed85f34c446a2f4087fb3aa8e8b
Hugo Caruso <hugoismyname@yahoo.com>
2020-10-08 17:31:38 -0400

    testing29

--- config/settings/base.py
+++ config/settings/base.py
@@ -304,7 +304,7 @@ if USE_S3:
     # s3 static settings
     AWS_LOCATION = "static"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    # STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     STATIC_URL = "/static/"
     MEDIA_URL = "/media/"
