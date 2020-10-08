401d979072160a622b8ba8686964fb4b0c98cf89
Hugo Caruso <hugoismyname@yahoo.com>
2020-10-08 18:09:56 -0400

    test101

--- config/settings/base.py
+++ config/settings/base.py
@@ -302,9 +302,8 @@ if USE_S3:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     # s3 static settings
-    AWS_LOCATION = "static"
     STATIC_URL = "https://edhtracker.s3.us-east-2.amazonaws.com"
-    # STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     STATIC_URL = "/static/"
     MEDIA_URL = "/media/"
