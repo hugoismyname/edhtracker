914a838f3ce27dca630ea218c21e83874dac783c
Hugo Caruso <hugoismyname@yahoo.com>
2020-10-09 14:55:14 -0400

    test109

--- config/settings/base.py
+++ config/settings/base.py
@@ -290,26 +290,17 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
+STATIC_URL = "/static/"
+MEDIA_URL = "/images/"
+# AWS_LOCATION = "edhtracker"
+AWS_S3_REGION_NAME = "us-east-2"
+AWS_ACCESS_KEY_ID = "AKIAUYUPP7AIHPSXHUFF"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "edhtracker"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-USE_S3 = env.bool("USE_S3", True)
-
-if USE_S3:
-    # aws settings
-    AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = "public-read"
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-    # s3 static settings
-    STATIC_URL = "https://edhtracker.s3.us-east-2.amazonaws.com/"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-else:
-    STATIC_URL = "/static/"
-    MEDIA_URL = "/media/"
-
-MEDIA_ROOT = "/media/"
-MEDIA_URL = "/media/"
-
-STATIC_ROOT = "/staticrooojfod/"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
